0a6466e6810940d8383008de4b070acb6325e474
VovaGld <vova.gld05@gmail.com>
2022-02-01 23:56:55 +0300

    сума

--- Praktuchna10/SumMas.cpp
+++ Praktuchna10/SumMas.cpp
@@ -1,1 +1,9 @@
 #include <iostream>
+
+void SumMas(int** A, int** B, int** C, int rowC, int colC) {
+	for (int i = 0; i < rowC; i++) {
+		for (int j = 0; j < colC; j++) {
+			C[i][j] = A[i][j] + B[i][j];
+		}
+	}
+}
