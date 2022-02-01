f4d2b46fc67c1066c651a8b1a91e23e5e1f20749
VovaGld <vova.gld05@gmail.com>
2022-02-01 15:51:11 +0300

    Main

--- Praktuchna10/Praktuchna10.cpp
+++ Praktuchna10/Praktuchna10.cpp
@@ -5,7 +5,19 @@
 int main()
 {
     setlocale(LC_CTYPE, "ukr");
-    int row, col;
-    int A[3][3];
+    int rowA, colA, rowB, colB;
+    rowA = size();
+    colA = size();
+    int** A = new int*[colA];
+    for (int i = 0; i < colA; i++) {
+        A[i] = new int[rowA];
+    }
+    rowB = size();
+    colB = size();
+    int** B = new int* [colB];
+    for (int i = 0; i < colB; i++) {
+        B[i] = new int[rowB];
+    }
+    int **C = new int*
 }
 
