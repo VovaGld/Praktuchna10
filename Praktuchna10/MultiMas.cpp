f3c89015cc2fd6819a35041524db0771eeff01a4
VovaGld <vova.gld05@gmail.com>
2022-02-04 15:23:19 +0300

    gdfgdfbvn

--- Praktuchna10/MultiMas.cpp
+++ Praktuchna10/MultiMas.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-void MultiMas(int** A, int** B, int** Multi, int rowMulti, int colMulti, int colA, int rowA, int colB, int rowB) {
-	int cola = colA, rowa = 0, colb = 0, rowb = rowB, n = 0, k = 0, M = 0;
+void MultiMas(int** A2, int** B2, int** Multi, int rowMulti, int colMulti, int colA2, int rowA2, int colB2, int rowB2) {
 	for (int i = 0; i < rowMulti; i++) {
 		for (int j = 0; j < colMulti; j++) {
 			Multi[i][j] = 0;
-			for (int c = 0; c < colA;c++) {
-				Multi[i][j] += A[i][c] * B[c][j];
+			for (int c = 0; c < colA2;c++) {
+				Multi[i][j] += A2[i][c] * B2[c][j];
 			}
 		}
 	}
