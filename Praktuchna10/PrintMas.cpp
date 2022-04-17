f6d8da23cf42a84d7d4d13a878a9b37ac660e0ce
VovaGld <vova.gld05@gmail.com>
2022-04-17 14:55:45 +0300

    rth

--- Praktuchna10/PrintMas.cpp
+++ Praktuchna10/PrintMas.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-void PrintB2(int** Mas, int row, int col) {
+void PrintMas(int** Mas, int row, int col) {
 	for (int i = 0; i < row; i++) {
 		for (int k = 0; k < col; k++) {
 			printf("%6d", Mas[i][k]);
